6ce76db222f1989d01ac2f5f2085fc78f32e9e99
A-Kija <arvydas.kijakauskas@gmail.com>
2023-12-19 12:28:03 +0200

    028

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import './buttons.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Sq from './Components/028/Sq';
 import randomColor from './Functions/randomColor';
 import { v4 as uuidv4 } from 'uuid';
@@ -15,17 +15,34 @@ export default function App() {
     const [sq2, setSq2] = useState('#444444');
     const [sq1, setSq1] = useState('#444444');
 
+    const [sync, setSync] = useState(false);
+
+    useEffect(_ => {
+        console.log('Squres are changed');
+        if (sync) {
+            setSquares(s => s.map(s => ({ ...s, show: true })));
+            setSync(false);
+        }
+    }, [squares]);
+
+
     const add = _ => {
         setSquares(s => [...s,
         {
             color: randomColor(),
-            id: uuidv4()
+            id: uuidv4(),
+            show: true
         }
-    ]);
+        ]);
     }
 
     const reset = _ => {
-        setSquares([]);
+        setSquares(s => s.map(s => ({ ...s, show: false })));
+    }
+
+    const syncSpin = _ => {
+        setSquares(s => s.map(s => ({ ...s, show: false })));
+        setSync(true);
     }
 
 
@@ -36,12 +53,14 @@ export default function App() {
                 <h1>This is STATE part II</h1>
                 <div className="squares">
                     {
-                        squares.map((s, i) => <Sq setSquares={setSquares} square={s} key={i} />)
+                        squares.map((s, i) => s.show ? <Sq setSquares={setSquares} square={s} key={i} /> : null)
                     }
                 </div>
                 <div className="buttons">
                     <button className="black" onClick={add}>+</button>
                     <button className="red" onClick={reset}>0</button>
+                    <button className="green" onClick={_ => setSquares(s => s.map(s => ({ ...s, show: true })))}>*</button>
+                    <button className="yellow" onClick={syncSpin}>sync</button>
                 </div>
                 <div className="squares">
                     <BigSq1 sq1={sq1} setSq1={setSq2} />
