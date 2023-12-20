3196ead4b372c9783c2ac787a6d23c6b74ce0741
A-Kija <arvydas.kijakauskas@gmail.com>
2023-12-20 12:30:13 +0200

    029

--- r1/src/Components/029/ColorCircle.jsx
+++ r1/src/Components/029/ColorCircle.jsx
@@ -4,6 +4,7 @@ export default function ColorCircle({ color }) {
         <div className="color-circle" style={{
             backgroundColor: color + '66',
             border: '5px dotted ' + color,
+            animation: 'spin 20s linear infinite'
         }}
         ></div>
     );
