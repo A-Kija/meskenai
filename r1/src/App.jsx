eb79516e8e671bea9a3b5c2b3041b19829648310
A-Kija <arvydas.kijakauskas@gmail.com>
2023-12-27 12:49:17 +0200

    031

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -9,12 +9,26 @@ export default function App() {
     const [dog, setDog] = useState('');
     const [bird, setBird] = useState('');
 
+    useEffect(_ => {
+        let animals = JSON.parse(localStorage.getItem('animals'));
+        if (null === animals)  {
+            return;
+        }
+        setCat(animals.cat);
+        setDog(animals.dog);
+        setBird(animals.bird);
+    }, []);
+
     const saveCat = _ => {
         localStorage.setItem('cat', cat);
     }
 
     const removeCat = _ => {
         localStorage.removeItem('cat');
+    }
+
+    const getCat = _ => {
+        setCat(localStorage.getItem('cat'));
     }
 
     const addDog = _ => {
@@ -37,6 +51,10 @@ export default function App() {
         localStorage.clear();
     }
 
+    const addAll = _ => {
+        localStorage.setItem('animals', JSON.stringify({cat, dog, bird}));
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -49,13 +67,17 @@ export default function App() {
                     <input type="text" name="dog" value={dog} onChange={e => setDog(e.target.value)} />
                     <label>Bird</label>
                     <input type="text" name="bird" value={bird} onChange={e => setBird(e.target.value)} />
+                    <input type="text" name="name" autocomplete="off" />
                     <div className="buttons">
-                        <button className="blue" onClick={saveCat}>Save Cat</button>
+                        <button className="black" onClick={saveCat}>Add Cat</button>
                         <button className="red" onClick={removeCat}>Remove Cat</button>
+                        <button className="black" onClick={getCat}>Get Cat</button>
                         <button className="yellow" onClick={addDog}>Add Dog</button>
                         <button className="red" onClick={removeDog}>Remove Dog</button>
                         <button className="green" onClick={addBird}>Add Bird</button>
                         <button className="red" onClick={removeBird}>Remove Bird</button>
+
+                        <button className="white" onClick={addAll}>Add All</button>
 
                         <button className="red" onClick={clear}>Clear</button>
                     </div>
