bf0c7133f1b1494fe47700aedfa30ed3b45166a7
A-Kija <arvydas.kijakauskas@gmail.com>
2024-02-29 11:17:26 +0200

    067A

--- library/front/src/Contexts/Messages.jsx
+++ library/front/src/Contexts/Messages.jsx
@@ -1,31 +1,40 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useCallback } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export const MessagesContext = createContext();
 
 export const MessagesProvider = ({ children }) => {
 
-   const [messages, setMessages] = useState([
-        { text: 'Welcome to the Library', type: 'primary' },
-        { text: 'You are good boy/girl', type: 'success'}
-   ]);
+    const [messages, setMessages] = useState([]);
+
+
+    const addMessage = useCallback(({text, type}) => {
+        const id = uuidv4();
+        setMessages(m => [...m, { text, type, id }]);
+        setTimeout(_ => {
+            setMessages(m => m.filter(m => m.id !== id));
+        }, 5000);
+    }, []);
+
 
     return (
         <MessagesContext.Provider value={{
-            messages, setMessages
+            addMessage
         }}>
             <>
-            <div className="messages">
-                {
-                    messages.map((message, i) => (
-                        <div key={i} className={`alert alert-${message.type}`} role="alert">
-                           {message.text}
-                        </div>
-                    ))
-                }
-            </div>
-            {children}
+                <div className="messages">
+                    {
+                        messages.map(message => (
+                            <div key={message.id} className={`alert alert-${message.type}`} 
+                            role="alert" onClick={_ => setMessages(m => m.filter(m => m.id !== message.id))}>
+                                {message.text}
+                            </div>
+                        ))
+                    }
+                </div>
+                {children}
             </>
-            
+
         </MessagesContext.Provider>
     );
 }
